b67cd4f2e1d3747944069f04f0f80fb1c9bb4904
anittajose <anittajose94@gmail.com>
2023-08-17 19:47:21 +0530

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,75 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const [itemText, setItemText] = useState('');
+  const [itemAssignee, setItemAssignee] = useState('');
+  const [listItems, setListItems] = useState([]);
+
+  //add new todo item to database
+  const addItem = async (e) => {
+    e.preventDefault();
+   
+    try{
+      const res = await axios.post('http://localhost:5500/api/item', {item: e.target.item.value, assignee:e.target.assignee.value})
+      setListItems(prev => [...prev, res.data]);
+      setItemText('');
+      setItemAssignee('');
+    }catch(err){
+      console.log(err);
+    }
+  }
+
+  //Create function to fetch all todo items from database
+  useEffect(()=>{
+    const getItemsList = async () => {
+      try{
+        const res = await axios.get('http://localhost:5500/api/items')
+        setListItems(res.data);
+        console.log('render')
+      }catch(err){
+        console.log(err);
+      }
+    }
+    getItemsList()
+  },[]);
+
+  // Delete item when click on delete
+  const deleteItem = async (id) => {
+    try{
+      const res = await axios.delete(`http://localhost:5500/api/item/${id}`)
+      const newListItems = listItems.filter(item=> item._id !== id);
+      setListItems(newListItems);
+    }catch(err){
+      console.log(err);
+    }
+  }
+
+  return (
+    <div className="App">
+      <div className="todo-heading">Sprint Board</div>
+      <form className="form" onSubmit={e => addItem(e)}>
+        <input type="text" id="item" name="item" placeholder='Enter Ticket Description' aria-label="ticket description" onChange={e => {setItemText(e.target.value)} } value={itemText} />
+        <input type="text" id="assignee" name="assignee" placeholder=' Enter Assignee' aria-label="assignee" onChange={e => {setItemAssignee(e.target.value)} } value={itemAssignee} />
+        <button type="submit">Add To Board</button>
+      </form>
+      <div className="todo-listItems">
+        {
+          listItems.map(item => (
+          <div className="todo-item">
+               <>
+                  <p className="item-content">{item.item}</p>
+                  <span>Assignee: {item.assignee}</span>
+                  <button className="delete-item" onClick={()=>{deleteItem(item._id)}}>Delete</button>
+                </>
+          </div>
+          ))
+        }
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/App.test.js
@@ -0,0 +1,28 @@
+import { render, screen,waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+import App from './App';
+
+describe("App", () => {
+
+  it("Render Sprint board elements on screen", () => {
+     render(<App />);
+     const heading = screen.getByText(/Sprint Board/i);
+     expect(heading).toBeInTheDocument();
+     const ticketDescription = screen.getByRole("textbox", { name: /ticket description/i });
+     expect(ticketDescription).toBeInTheDocument();
+     const assignee = screen.getByRole("textbox", { name: /assignee/i });
+     expect(assignee).toBeInTheDocument();
+     const addButton = screen.getByRole("button", { name: /add to board/i });
+     expect(addButton).toBeInTheDocument;
+  });
+
+  it("User Interactions - Adding Ticket description and Assignee", async () => {
+     render(<App />);
+     const ticketDescription = screen.getByRole("textbox", { name: /ticket description/i });
+     userEvent.type(ticketDescription, 'Create Login');
+     const assignee = screen.getByRole("textbox", { name: /assignee/i });
+     userEvent.type(assignee, 'Ankit');
+     const addButton = screen.getByRole("button", { name: /add to board/i });
+     userEvent.click(addButton);
+  });
+});
